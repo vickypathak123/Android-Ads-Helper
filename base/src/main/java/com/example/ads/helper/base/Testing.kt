bbfb0a09d3e460a789d8f989d088f6a7453e1a56
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2022-01-01 12:37:44 +0530

    Add Extra Module For Testing

---
+++ base/src/main/java/com/example/ads/helper/base/Testing.kt
@@ -0,0 +1,10 @@
+package com.example.ads.helper.base
+
+import android.util.Log
+
+class Testing {
+    private val TAG: String = javaClass.simpleName
+    fun logTesting() {
+        Log.e(TAG, "logTesting: ")
+    }
+}
