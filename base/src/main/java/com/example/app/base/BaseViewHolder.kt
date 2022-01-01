46efe855703383ae60ba859401660a5dd6e12090
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2022-01-01 09:58:57 +0530

    Initial commit

--- base/src/main/java/com/example/app/base/BaseViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app.base
-
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
-
-abstract class BaseViewHolder<VB: ViewBinding>(val fBinding: VB): RecyclerView.ViewHolder(fBinding.root)
